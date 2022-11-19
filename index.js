3a708471a1e350ed68f100877eb287c23f6bda5e
Nikhil Bachhav <nikhil.bachhav@hotmail.com>
2022-11-19 18:01:35 +0530

    vercel.json added

--- index.js
+++ index.js
@@ -11,21 +11,25 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-const db = require("./models/db");
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch((err) => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-require("./routes/routes")(app);
+// const db = require("./models/db");
+// db.mongoose
+//   .connect(db.url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to the database!");
+//   })
+//   .catch((err) => {
+//     console.log("Cannot connect to the database!", err);
+//     process.exit();
+//   });
+
+// require("./routes/routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
